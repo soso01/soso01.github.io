7fab77c9f797470c05962cfbfe4660e69c981496
soso01 <6125024@gmail.com>
2021-01-15 15:29:48 +0900

    Updates

--- sw.js
+++ sw.js
@@ -26,10 +26,10 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-60357e5df40b4bd59b34.js"
+    "url": "webpack-runtime-1aa1a9dc53ff3f70af0f.js"
   },
   {
-    "url": "styles.3cd90cd225867c59d389.css"
+    "url": "styles.42e756c2f2787e453ca2.css"
   },
   {
     "url": "styles-e9d24b1846c7d6eb9685.js"
@@ -45,7 +45,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "0da04dc7daa9b994b675585a4c1d1e76"
+    "revision": "4f9a02ecd05a0ed3215fc5d62480a2f3"
   },
   {
     "url": "polyfill-31bd25ec256a9117a66a.js"
